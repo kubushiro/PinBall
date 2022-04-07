1be4ddf3e71acac483211a2d3fc9601c0fcf2a7c
kubushiro <102495411+kubushiro@users.noreply.github.com>
2022-04-07 15:48:07 +0900

    Lesson5_kadai
    課題の作成

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -12,12 +12,22 @@ public class BallController : MonoBehaviour
     //ゲームオーバを表示するテキスト
     private GameObject gameoverText;
 
+    //課題：得点テキスト
+    private GameObject scoredisplayText;
+
+    private int ScoreText = 0;
+
+    private string objName;
+
     // Start is called before the first frame update
     void Start()
     {
         //シーンのGameoverTextオブジェクトを取得
         this.gameoverText = GameObject.Find("GameOverText");
-        
+
+        //課題：シーンのScoreDisplayオブジェクトを取得
+        this.scoredisplayText = GameObject.Find("ScoreDisplay");
+        this.scoredisplayText.GetComponent<Text>().text = "得点："+ ScoreText ;
     }
 
     // Update is called once per frame
@@ -29,6 +39,29 @@ public class BallController : MonoBehaviour
             //GameoverTextにゲームオーバを表示
             this.gameoverText.GetComponent<Text>().text = "Game Over";
         }
-        
+
+        this.scoredisplayText.GetComponent<Text>().text = "得点：" + ScoreText;
+    }
+    void OnCollisionEnter(Collision other)
+    {
+
+        //角度を180に設定
+        this.objName = other.gameObject.tag;
+        if (objName == "SmallStarTag")
+        {
+            this.ScoreText += 50;
+        }
+        else if (objName == "LargeStarTag")
+        {
+            this.ScoreText += 70;
+        }
+        else if (objName == "SmallCloudTag")
+        {
+            this.ScoreText += 20;
+        }
+        else if (objName == "LargeCloudTag")
+        {
+            this.ScoreText += 30;
+        }
     }
 }
